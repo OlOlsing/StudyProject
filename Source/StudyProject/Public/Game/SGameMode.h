3e9752a021cd9262cd2d2117f9023e679f2abaf3
OlOlsing <gomdi202@naver.com>
2024-03-10 15:17:39 +0900

    feat : Pawn 실습

---
+++ Source/StudyProject/Public/Game/SGameMode.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDYPROJECT_API ASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+    ASGameMode();
+
+	
+};
